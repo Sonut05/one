b98fa975149d823cb71ce9fe09fcbbf5b42d945e
Sonut05 <sonutiwari042006@gmail.com>
2025-05-25 13:30:51 +0530

    Added

---
+++ CONDITIONAL/triangle.js
@@ -0,0 +1,7 @@
+if (a === b && b === c) {
+    console.log("Equilateral Triangle");
+} else if (a === b || b === c || a === c) {
+    console.log("Isosceles Triangle");
+} else {
+    console.log("Scalene Triangle");
+}
